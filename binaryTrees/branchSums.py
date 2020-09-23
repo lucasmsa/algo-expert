3b7b642438200ca02d1f30ea49ff2642ae83b34d
Lucas Moreira <lucasmsea@outlook.com>
2020-09-23 20:36:04 -0300

    finish branchSums on Binary trees

---
+++ binaryTrees/branchSums.py
@@ -0,0 +1,23 @@
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def branchSums(root: BinaryTree):
+	sumsArray = []   
+	return branchSumsHelper(root, root.value, sumsArray)
+    
+def branchSumsHelper(root: BinaryTree, branchSum: int, sumsArray: list):
+
+	if root.left:
+		branchSumsHelper(root.left, branchSum + root.left.value, sumsArray)   
+
+	if root.right:
+		branchSumsHelper(root.right, branchSum + root.right.value, sumsArray)
+
+	if root.left is None and root.right is None:
+		sumsArray.append(branchSum)
+		return sumsArray
+
+	return sumsArray
