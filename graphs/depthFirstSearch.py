047caf3c451216daf7964cc59a10922abb475dfc
Lucas Moreira <lucasmsea@outlook.com>
2020-09-26 23:02:23 -0300

    add depthFirstSearch -py

---
+++ graphs/depthFirstSearch.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        array.append(self.name)
+        for child in self.children:
+            self.name = children.name
+            self.children = child.children
+            self.depthFirstSearch(array)
+
+        return array
