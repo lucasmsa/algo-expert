70a7721f09ebf1258a50d1551eb6d176dda051c1
Lucas Moreira <lucasmsea@outlook.com>
2020-10-14 11:39:25 -0300

    decrease one iteration from selection sort

--- sorting/selectionSort/selectionSort.py
+++ sorting/selectionSort/selectionSort.py
@@ -1,5 +1,5 @@
 def selectionSort(array: list):
-  for i in range(len(array)):
+  for i in range(len(array) - 1):
     j = i + 1
     swapIdx = i
     
@@ -15,3 +15,5 @@ def selectionSort(array: list):
   
 def swap(array: list, i: int, j: int):
   array[i], array[j] = array[j], array[i]
+
+print(selectionSort([8, 5, 2, 9, 5, 6, 3]))
