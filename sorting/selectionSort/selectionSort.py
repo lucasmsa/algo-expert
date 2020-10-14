02a030c9b3e806d5ddb84fb543b6410b6ab86fff
Lucas Moreira <lucasmsea@outlook.com>
2020-10-14 11:35:19 -0300

    finish selection sort

---
+++ sorting/selectionSort/selectionSort.py
@@ -0,0 +1,17 @@
+def selectionSort(array: list):
+  for i in range(len(array)):
+    j = i + 1
+    swapIdx = i
+    
+    while j < len(array):
+      if array[j] < array[swapIdx]:
+        swapIdx = j
+      j += 1
+    
+    if swapIdx != i:
+      swap(array, i, swapIdx)
+    
+  return array
+  
+def swap(array: list, i: int, j: int):
+  array[i], array[j] = array[j], array[i]
