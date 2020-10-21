290ee81b66955d1afd00d5a2271457a6209f74d9
Lucas Moreira <lucasmsea@outlook.com>
2020-10-21 15:30:45 -0300

    complete longestPeak -ts

---
+++ arrays/longestPeak/longestPeak.ts
@@ -0,0 +1,25 @@
+export function longestPeak(array: number[]) {
+  let longest = 0, possibleLongest = 0, peak = false
+
+  for (let i = 0; i < array.length; i++) {
+    if ((array[i + 1] > array[i]) && !peak) {
+      possibleLongest += 1
+    }
+
+    else if ((array[i + 1] < array[i]) && (possibleLongest > 0)) {
+      peak = true
+      possibleLongest += 1
+    }
+
+    else {
+      (possibleLongest > longest && possibleLongest > 1 && peak) 
+      && (longest = possibleLongest + 1)
+
+      array[i + 1] > array[i] ? possibleLongest = 1 : possibleLongest = 0
+      
+      peak = false
+    }
+  }
+
+  return longest
+}
