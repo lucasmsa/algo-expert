2d740bd839c5c495a20c2a8075125e2c6c3da958
Lucas Moreira <lucasmsea@outlook.com>
2020-10-26 12:04:30 -0300

    finish threeNumberSum -py

---
+++ arrays/threeNumberSum/threeNumberSum.ts
@@ -0,0 +1,28 @@
+type Triplet = [number, number, number];
+
+// O(n^2) Time | O(n) Space 
+export function threeNumberSum(array: number[], targetSum: number): Triplet[] {
+  array.sort((n1,n2) => n1 - n2);
+
+  let center = 1
+  let right = array.length - 1
+  let tripletsAnswer = [] as Triplet[]
+
+  for (let i = 0; i < array.length - 2; i++) {
+      center = i + 1
+      right = array.length - 1
+
+      while (center < right) {
+        if (targetSum < array[right] + array[i] + array[center]) {
+          right -= 1
+        } else if (targetSum > array[i] + array[right] + array[center]) {
+          center += 1
+        } else {
+          let triplet = [array[i], array[center], array[right]] as Triplet
+          tripletsAnswer.push(triplet)
+         	center += 1
+      }
+    }
+  }
+  return tripletsAnswer;
+}
--- graphs/breadthFirstSearch/breadthFirstSearch.py
+++ graphs/breadthFirstSearch/breadthFirstSearch.py
@@ -10,11 +10,11 @@ class Node:
     def breadthFirstSearch(self, array: list):
         graph = self
         array.append(self.name)
-        queue = [self.name]
+        queue = [Node(self.name)]
 
-        while queue:
+        while queue or graph.children:
             for child in graph.children:
-                array.append(child)
+                array.append(child.name)
                 queue.append(child)
                 
             graph = queue.pop(0)
