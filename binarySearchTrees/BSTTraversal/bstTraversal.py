be290b1bd48868c3ea703282337f345444dbf54a
Lucas Moreira <lucasmsea@outlook.com>
2020-11-07 17:21:48 -0300

    add space-time complexity to bst traversal

--- binarySearchTrees/BSTTraversal/bstTraversal.py
+++ binarySearchTrees/BSTTraversal/bstTraversal.py
@@ -1,10 +1,14 @@
+# O(n) Time 
+# O(n) Space -> Because the answer is an array with all BST node values, 
+# however if the answer didn't involve storing values it would be
+# O(d) Space -> Where d is the depth of the binary tree, because of the call stack
 def inOrderTraverse(tree, array):
   if tree:
     inOrderTraverse(tree.left, array)
     array.append(tree.value)
     inOrderTraverse(tree.right, array)
   else: return
-  
+
   return array
 
 def preOrderTraverse(tree, array):
@@ -13,7 +17,7 @@ def preOrderTraverse(tree, array):
     preOrderTraverse(tree.left, array)
     preOrderTraverse(tree.right, array)
   else: return
-  
+
   return array
 
 
@@ -23,5 +27,5 @@ def postOrderTraverse(tree, array):
     postOrderTraverse(tree.right, array)
     array.append(tree.value)
   else: return
-  
+
   return array
