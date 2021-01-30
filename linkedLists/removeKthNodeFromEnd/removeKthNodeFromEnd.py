4a7d22a88189f2fa3b8852056472f2fe77ea497e
Lucas Moreira <lucasmsea@outlook.com>
2021-01-30 16:56:05 -0300

    finish removeKthNodeFromEnd -py

---
+++ linkedLists/removeKthNodeFromEnd/removeKthNodeFromEnd.py
@@ -0,0 +1,30 @@
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+		
+def removeKthNodeFromEnd(head, k):
+	current_node = head
+	linked_list_size = 0
+	while current_node:
+		current_node = current_node.next
+		linked_list_size += 1
+	current_node = head
+	previous_node = None
+	next_node = None
+	kth_node_position = linked_list_size - k
+	idx = 0
+	while idx < kth_node_position:
+		if idx == kth_node_position - 1:
+			previous_node = current_node
+		current_node = current_node.next
+		idx += 1
+		
+	if idx == 0: 
+		head.value = current_node.next.value
+		head.next = current_node.next.next
+	else:
+		next_node = current_node.next
+		current_node.next = None
+		previous_node.next = next_node
