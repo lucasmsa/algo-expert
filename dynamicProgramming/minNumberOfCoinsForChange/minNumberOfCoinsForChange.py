81803889635c0388516a0930e6af8a4e5d63d875
Lucas Moreira <lucasmsea@outlook.com>
2021-03-06 21:33:57 -0300

    remove comments

--- dynamicProgramming/minNumberOfCoinsForChange/minNumberOfCoinsForChange.py
+++ dynamicProgramming/minNumberOfCoinsForChange/minNumberOfCoinsForChange.py
@@ -8,18 +8,4 @@ def minNumberOfCoinsForChange(n, denoms):
                 min_number_of_coins[idx] = min(min_number_of_coins[idx - denom] + 1, min_number_of_coins[idx])
     return min_number_of_coins[n] if min_number_of_coins[n] != float('inf') else -1
 
-
 minNumberOfCoinsForChange(7, [1, 5, 10])
-
-# [0, inf, inf, inf, inf, inf, inf, inf]
-# [1, 1, 2, 3, 4, 5, 6, 7]
-#           |  |
-#       a[3 - 1] + 1 = 3
-#              |
-#          a[4 - 1] + 1 = 4
-
-# [0, 1, 2, 3, 4, 1, 2, 3]
-#                 |  |
-#              min(a[5 - 5] + 1 = ->[1]<-, a[5 - 1] + 1 = [5])
-#                    |
-#                 min(a[6 - 5] + 1 = ->[2]<-, a[6 - 1] + 1 = 6)
