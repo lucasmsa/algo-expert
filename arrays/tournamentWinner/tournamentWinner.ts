cc852528a9228bed3d934cab30799f73434f1415
Lucas Moreira <lucasmsea@outlook.com>
2021-05-26 15:53:55 -0300

    finish tournamentWinner -ts

---
+++ arrays/tournamentWinner/tournamentWinner.ts
@@ -0,0 +1,17 @@
+interface ILanguageScore {
+  [language: string]: number
+}
+
+// O(n) Time | O(T) Space
+// Where n is the number of matches played and T is the number of Teams
+function tournamentWinner(competitions: string[][], results: number[]) {
+  const languageScore = {} as ILanguageScore
+  for (let idx = 0; idx < results.length; idx++) {
+    const [homeTeam, awayTeam] = competitions[idx]
+    const winningTeam = results[idx] === 1 ? homeTeam : awayTeam
+
+    languageScore[winningTeam] = languageScore[winningTeam] + 3 || 3
+  }
+
+  return Object.keys(languageScore).reduce((a, b) => languageScore[a] > languageScore[b] ? a : b);
+}
--- greedyAlgorithms/classPhotos/classPhotos.py
+++ greedyAlgorithms/classPhotos/classPhotos.py
@@ -1,17 +1,13 @@
+# O(nlog(n)) Time | O(1) Space
 def classPhotos(redShirtHeights, blueShirtHeights):
     redShirtHeights.sort()
     blueShirtHeights.sort()
-    if redShirtHeights[0] == blueShirtHeights[0]:
-        return False
-    else:
-        tallestStudents = "RED" if max(
-            redShirtHeights[0], blueShirtHeights[0]) == redShirtHeights[0] else "BLUE"
 
-        for idx in range(1, len(redShirtHeights)):
-            if tallestStudents == "RED" and redShirtHeights[idx] <= blueShirtHeights[idx] \
-                    or tallestStudents == "BLUE" and blueShirtHeights[idx] <= redShirtHeights[idx]:
-                return False
+    tallestStudents = "RED" if max(
+        redShirtHeights[0], blueShirtHeights[0]) == redShirtHeights[0] else "BLUE"
+
+    for idx in range(len(redShirtHeights)):
+        if tallestStudents == "RED" and redShirtHeights[idx] <= blueShirtHeights[idx] \
+                or tallestStudents == "BLUE" and blueShirtHeights[idx] <= redShirtHeights[idx]:
+            return False
     return True
-
-
-print(classPhotos([5, 8, 1, 3, 4], [6, 9, 2, 4, 5]))
